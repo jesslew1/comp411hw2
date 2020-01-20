2ec1ae6d9c740f9b99b95832e086f07934f95324
jessica <jesslew@ad.unc.edu>
2020-01-20 16:57:17 -0500

    initial commit for 3-5

---
+++ ex3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <float.h>
+
+int main()
+{
+    int arr[6];
+
+    while (1) {
+        printf("Enter 6 integers: \n");
+        for (int i = 0; i < 6; i++) {
+            scanf("%i", &arr[i]);
+        }
+        printf("1234567890bb1234567890 \n");
+        for (int i = 0; i < 3; i++){
+            printf("%*i  ", 10, arr[i*2]);
+            printf("%*i \n", 10, arr[i*2 +1]);
+
+        }
+    }
+}
---
+++ ex5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <float.h>
+
+int main()
+{
+    int height;
+    int width;
+
+    while (1) {
+        printf("Please enter width and height:\n");
+        scanf("%d", &width);
+        if (width == 0){
+            printf("End");
+            break;
+        }
+        scanf("%d", &height);
+          if (height == 0){
+            printf("End");
+            break;
+        }
+        for (int i =0; i < height; i++){
+
+            // base case for top and bottom rows
+            if ( i == 0 || i == height-1){
+                printf("+");
+                for (int k = 1; k < width -1; k++){
+                    printf("-");
+                }
+                if (width != 1){
+                    printf("+\n");
+                } else {
+                    printf("\n");
+                }    
+            } else {
+                for (int j = 0; j < width; j++){
+                    if (j != 0 && j != width -1){
+                        printf("~");
+                    } else if (j ==0) {
+                        printf("|");
+                    } else {
+                        printf("|\n");
+                    }
+                }
+            }
+
+        }
+    }
+}
