4a4ab2972e26ffb65e59ba888cef2e37d94ccd0f
jessica <jesslew@ad.unc.edu>
2020-01-20 15:58:55 -0500

    nitial commit of first 2 hw

---
+++ ex1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define PI 3.14159
+
+int main()
+{
+    int r;
+
+    while (1) {
+    printf("Enter a number from 1 to 20: \n");
+    scanf("%d", &r);
+        if (r > 20 || r < 1){ 
+            printf("Number is not in the range from 1 to 20 \n");
+        } else {
+            printf("Here are the first %d ordinal numbers: \n", r);
+            printf("1st \n");
+            if (r >= 2) {
+                printf("2nd \n");
+            } if (r >=3){
+                printf("3rd \n");
+            } for (int i =4; i <= r; i++){
+                printf("%dth \n",i);
+            }
+
+        }
+    }
+}
---
+++ ex2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <float.h>
+
+int main()
+{
+    double r;
+    double sum = 0;
+    double min = DBL_MAX;
+    double max = - DBL_MAX;
+    double product = 1;
+
+    while (1) {
+        printf("Enter 10 floating-point numbers: \n");
+        for (int i = 0; i < 10; i++) {
+            scanf("%lf", &r);
+            if (r > max){
+                max = r;
+            }
+            if (r < min){
+                min = r;
+            }
+            sum+=r;
+            product*=r;
+        }
+        printf("Sum is %.5lf \n",sum);
+        printf("Min is %.5lf \n", min);
+        printf("Max is %.5lf \n", max);
+        printf("Product is %.5lf \n", product);
+    }
+}
