a3c7b63aeacc5d17532d0f635c33f3df3f3241a7
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-01-21 13:52:27 -0500

    took out all while loops and changed spacing

--- ex3.c
+++ ex3.c
@@ -4,17 +4,17 @@
 int main()
 {
     int arr[6];
-    while (1) {
+    // while (1) {
 
-        printf("Enter 6 integers: \n");
+        printf("Enter 6 integers:\n");
         for (int i = 0; i < 6; i++) {
             scanf("%i", &arr[i]);
         }
-        printf("1234567890bb1234567890 \n");
+        printf("1234567890bb1234567890\n");
         for (int i = 0; i < 3; i++){
             printf("%*i  ", 10, arr[i*2]);
             printf("%*i \n", 10, arr[i*2 +1]);
 
         }
-    }    
+    // }    
 }
--- ex4.c
+++ ex4.c
@@ -4,16 +4,16 @@
 int main()
 {
     int arr[6];
-    while (1){
-        printf("Enter 6 integers: \n");
+    // while (1){
+        printf("Enter 6 integers:\n");
         for (int i = 0; i < 6; i++) {
             scanf("%d", &arr[i]);
         }
-        printf("1234567890bb1234567890 \n");
+        printf("1234567890bb1234567890\n");
         for (int i = 0; i < 3; i++){
             printf("%*d  ", 10, arr[i*2]);
             printf("%*d \n", 10, arr[i*2 +1]);
 
         }
-    }    
+    // }    
 }
